362b5cf22ecfb3295f59596964240134e3c40bb6
DianeLantrann <diane.lantran@gmail.com>
2024-03-20 16:04:06 -0400

    image default and grid

--- BasicCodeLab_EnrichissementPerso6/app/src/main/java/com/example/basiccodelab_enrichissementperso6/MainActivity.kt
+++ BasicCodeLab_EnrichissementPerso6/app/src/main/java/com/example/basiccodelab_enrichissementperso6/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.basiccodelab_enrichissementperso6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,6 +18,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.basiccodelab_enrichissementperso6.ui.theme.BasicCodeLab_EnrichissementPerso6Theme
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.runtime.getValue
@@ -26,6 +30,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.foundation.Image
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,7 +46,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DisplayPlaylist("Android")
+                    //DisplayPlaylist("Android")
+                    BasicCodeLab_EnrichissementPerso6Theme {
+                        MyApp()
+                    }
                 }
             }
         }
@@ -48,39 +60,59 @@ class MainActivity : ComponentActivity() {
 fun DisplayPlaylist(name: String, modifier: Modifier = Modifier.fillMaxWidth()) {
     val expanded = remember { mutableStateOf(false) }
     val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val image_default = painterResource(R.drawable.defaultplaylist)
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 4.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)){
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)) {
-                Text(text = "Playlist ", color = Color.Gray,)
+        Box(
+            modifier = Modifier
+                .height(200.dp), // Set a fixed height for the cell
+                /*.padding(top = 8.dp),*/
+            contentAlignment = Alignment.Center
+        ){
+            Column(modifier = Modifier.padding(bottom = extraPadding), verticalArrangement = Arrangement.SpaceEvenly) {
+                Text(text = "Playlist ", color = Color.Gray ,)
+                Image(
+                    painter = image_default,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                )
                 Text(text = name, modifier=modifier, textAlign = TextAlign.Center)
+                
+                ElevatedButton(
+                    onClick = {
+                        expanded.value = !expanded.value
+                              },
+                    modifier = Modifier.padding(start = 16.dp)
+                ) {
+                    Text(if (expanded.value) "Currently listening" else "Listen")
+                }
                 if (expanded.value) {
                     Text(text= "nb of titles, total listing time", color = Color.Gray,)
                 }
             }
-            ElevatedButton(
-                onClick = {
-                    expanded.value = !expanded.value
-                          }, modifier = Modifier.padding(start = 16.dp)
-            ) {
-                Text(if (expanded.value) "Currently listening" else "Listen")
             }
         }
     }
-}
+
 @Composable
-fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("Chill with Clément", "Road trip with Etienne Clément and Emilie", "Party with Emilie")) {
+fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("Chill with Clément", "Road trip with Etienne Clément and Emilie", "Party with Emilie", "Cooking with Mathilde")) {
     Surface(
         modifier = modifier,
         color = MaterialTheme.colorScheme.background
     ) {
-        Column(modifier=modifier.padding(4.dp)) {
+        /*Column(modifier=modifier.padding(4.dp)) {
             for (name in names) {
                 DisplayPlaylist(name = name)
+            }
+        }*/
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            modifier = modifier.padding(4.dp)
+        ) {
+            items(names.size) { index ->
+                DisplayPlaylist(name = names[index])
             }
         }
     }
